a40d60c4fb35c7f5068cd27458eee02af6c3d229
Samuel Lucas <samuel.lucasmacedos@gmail.com>
2025-09-17 21:24:57 -0300

    Otimização ao chamar requisições, tratamento caso a api esteja offline.

--- App.xaml.cs
+++ App.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using Tasky.ViewModels;
@@ -15,10 +17,39 @@ namespace Tasky
     /// </summary>
     public partial class App : Application
     {
-        private void StartUpTasky(object sender, StartupEventArgs e)
-        {
-            var mainVm = new MainViewModel();
+        private static bool isApiOn = false;
 
+
+        private async void StartUpTasky(object sender, StartupEventArgs e)
+        {
+            HttpClient isApiOnClient = new HttpClient();
+            isApiOnClient.Timeout = TimeSpan.FromSeconds(5);
+
+            while (!isApiOn)
+            {
+                try
+                {
+                    var response = await isApiOnClient.GetAsync("http://localhost:3000/tasks/healthcheck");
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        isApiOn = true;
+                    }
+                    else
+                    {
+                        MessageBox.Show("API ERROR", "Api it's not on, trying to connect in 10 seconds...");
+                        await Task.Delay(10000);
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("\nAPI it's offline, trying again in 10 seconds...");
+                    MessageBox.Show("Api it's not on, trying to connect in 10 seconds...", "API ERROR");
+                    await Task.Delay(10000);
+                }
+            }
+
+            var mainVm = new MainViewModel();
             var window = new TasksHomePage(mainVm);
             window.Show();
         }
--- Database/Database.cs
+++ Database/Database.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -15,208 +21,173 @@ namespace Tasky.Database
 {
     public class Database : IDisposable
     {
-        private ObservableCollection<Models.Task> _viewModel;
-
         public void Dispose()
         {
         }
 
-        public static string fileName = "Database.json";
+
+        //public static string fileName = "Database.json";
 
         public static int size;
 
+        private static HttpClient client;
+
+
         public Database()
         {
-            WatchDatabase();
+            MapHttp();
+            //WatchDatabase();
         }
 
-        private void WatchDatabase()
+        private void MapHttp()
         {
-            if (!File.Exists(fileName))
-            {
-                File.WriteAllText(fileName, "[]");
-                //readJson();
-            }
-
-            ReadJson();
+            client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:3000/tasks");
         }
 
-        private void ReadJson()
+        //private void WatchDatabase()
+        //{
+        //    if (!File.Exists(fileName))
+        //    {
+        //        File.WriteAllText(fileName, "[]");
+        //    }
+
+        //    ReadJson();
+        //}
+
+        //private void ReadJson()
+        //{
+        //    if (File.Exists(fileName) && File.ReadAllLines(fileName).Length <= 2)
+        //    {
+        //        //DateTime data = string.Format("{0}/{1}/{2}", DateTime.Now.Day, DateTime.Now.Month, DateTime.Now.Year);
+        //        Models.Task toCreate = new Models.Task("Estudar", false, "Estudar programação", DateTime.Now.Date);
+        //        CreateTask(toCreate);
+        //    }
+        //}
+
+        public async Task<bool> CreateTask(Models.Task task)
         {
-            if (File.Exists(fileName) && File.ReadAllLines(fileName).Length <= 2)
+            try
             {
-                //DateTime data = string.Format("{0}/{1}/{2}", DateTime.Now.Day, DateTime.Now.Month, DateTime.Now.Year);
-                Models.Task toCreate = new Models.Task("Estudar", false, "Estudar programação", DateTime.Now.Date);
-                CreateTask(toCreate);
-            }
-        }
-        public async void CreateTask(Models.Task task)
-        {
-            //var tasks = await GetAllTasks();
-            //tasks.Add(task);
-            //SaveAll(tasks);
-
-
-
-            using (HttpClient client = new HttpClient())
-            {
-                try
+                size += 1;
+                task.TaskId = size;
+                var content = new StringContent(JsonConvert.SerializeObject(task), Encoding.UTF8, "application/json");
+                var response = await client.PostAsync(client.BaseAddress, content).ConfigureAwait(false);
+                if (response.EnsureSuccessStatusCode().StatusCode == System.Net.HttpStatusCode.OK)
                 {
-                    string url = "http://localhost:3000/tasks";
-                    //client.Timeout = new TimeSpan(5);
-                    size += 1;
-                    task.TaskId = size;
-                    var content = new StringContent(JsonConvert.SerializeObject(task), Encoding.UTF8, "application/json");
-                    //var x = JsonConvert.SerializeObject(task);
-                    var response = await client.PostAsync(url, content).ConfigureAwait(false);
-                    if (response.EnsureSuccessStatusCode().StatusCode == System.Net.HttpStatusCode.OK)
-                    {
-                        client.Dispose();
-
-                    }
-
+                    return true;
                 }
-                catch (Exception)
-                {
-                    Console.WriteLine("Error");
-                }
+                return false;
             }
-
-
-
-
-
+            catch (Exception ex) when (ex is HttpRequestException || ex is SocketException || ex is WebException)
+            {
+                MessageBox.Show("Application gonna restart");
+                Process.Start(Assembly.GetExecutingAssembly().Location);
+                Environment.Exit(0);
+                throw;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
         public async Task<ObservableCollection<Models.Task>> GetAllTasks()
         {
-            //var content = File.ReadAllText(fileName);   
-            ////return JsonConvert.DeserializeObject<List<Models.Task>>(content) ?? new List<Models.Task>();
-            //return JsonConvert.DeserializeObject<ObservableCollection<Models.Task>>(content) ?? new ObservableCollection<Models.Task>();
-
-            using (HttpClient client = new HttpClient())
+            try
             {
-                try
+                var response = await client.GetAsync(client.BaseAddress).ConfigureAwait(false);
+                if (response.EnsureSuccessStatusCode().StatusCode == System.Net.HttpStatusCode.OK)
                 {
-                    string url = "http://localhost:3000/tasks";
-                    //client.Timeout = new TimeSpan(5);
-                    var response = await client.GetAsync(url).ConfigureAwait(false);
-                    response.EnsureSuccessStatusCode();
-
-                    if (response.IsSuccessStatusCode)
-                    {
-                        var content = await response.Content.ReadAsStringAsync();
-                        size = JsonConvert.DeserializeObject<ObservableCollection<Models.Task>>(content).OrderBy(x => x.TaskId).Select(x => x.TaskId).Last();
-                        client.Dispose();
-                        return JsonConvert.DeserializeObject<ObservableCollection<Models.Task>>(content) ?? new ObservableCollection<Models.Task>();
-                    }
-
-                    throw new Exception();
+                    var content = await response.Content.ReadAsStringAsync();
+                    size = JsonConvert.DeserializeObject<ObservableCollection<Models.Task>>(content).OrderBy(x => x.TaskId).Select(x => x.TaskId).Last();
+                    //return JsonConvert.DeserializeObject<ObservableCollection<Models.Task>>(content) ?? new ObservableCollection<Models.Task>();                  
+                    return JsonConvert.DeserializeObject<ObservableCollection<Models.Task>>(content) ?? new ObservableCollection<Models.Task>();
                 }
-                catch (Exception)
-                {
-                    throw;
-                }
+                throw new Exception();
             }
-
-
-
+            catch (Exception ex) when (ex is HttpRequestException || ex is SocketException || ex is WebException)
+            {
+                MessageBox.Show("Application gonna restart");
+                Process.Start(Assembly.GetExecutingAssembly().Location);
+                Environment.Exit(0);
+                throw;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
-        public void SaveAll(ObservableCollection<Models.Task> tasks)
-        {
-            var content = JsonConvert.SerializeObject(tasks, Formatting.Indented);
-            File.WriteAllText(fileName, content);
-            GetAllTasks();
-        }
+        //public void SaveAll(ObservableCollection<Models.Task> tasks)
+        //{
+        //    var content = JsonConvert.SerializeObject(tasks, Formatting.Indented);
+        //    File.WriteAllText(fileName, content);
+        //    GetAllTasks();
+        //}
 
         public async void UpdateTask(string oldDesc, string oldName, string newDesc, string newName, DateTime newDate, DateTime oldDate)
         {
-            //var tasks = await GetAllTasks();
-
-            //var xt = tasks.FirstOrDefault(x => x.TaskName == oldName && x.TaskDesc == oldDesc && x.HourTask == oldDate);
-            //if (xt != null)
-            //{
-            //    xt.TaskName = newName;
-            //    xt.TaskDesc = newDesc;
-            //    xt.HourTask = newDate;
-            //    File.WriteAllText(fileName, JsonConvert.SerializeObject(tasks, Formatting.Indented));
-            //}
-            //else
-            //{
-            //    MessageBox.Show("Error");
-            //}
-
-
-            var tasks = await GetAllTasks();
-            var xt = tasks.FirstOrDefault(x => x.TaskName == oldName && x.TaskDesc == oldDesc && x.HourTask == oldDate);
-
-            if (xt != null)
+            try
             {
-                try
+                var tasks = await GetAllTasks();
+                var xt = tasks.FirstOrDefault(x => x.TaskName == oldName && x.TaskDesc == oldDesc && x.HourTask == oldDate);
+
+                if (xt != null)
                 {
-                    using (HttpClient client = new HttpClient())
+                    var payload = new Dictionary<string, dynamic>
                     {
-                        string url = $"http://localhost:3000/tasks?taskId={xt.TaskId}";
-                        var payload = new Dictionary<string, dynamic>
-                        {
-                            { "TaskName", newName },
-                            { "TaskDesc", newDesc },
-                            { "HourTask", newDate },
-
-                        };
-                        var content = new StringContent(JsonConvert.SerializeObject(payload), Encoding.UTF8, "application/json");
-                        var response = await client.PutAsync(url, content).ConfigureAwait(false);
-                        response.EnsureSuccessStatusCode();
-                        if (response.IsSuccessStatusCode)
-                        {
-                            client.Dispose();
-                        }
-                    }
+                        { "TaskName", newName },
+                        { "TaskDesc", newDesc },
+                        { "HourTask", newDate },
+                    };
+                    var content = new StringContent(JsonConvert.SerializeObject(payload), Encoding.UTF8, "application/json");
+                    var response = await client.PutAsync($"{client.BaseAddress}?taskId={xt.TaskId}", content).ConfigureAwait(false);
+                    response.EnsureSuccessStatusCode();
                 }
-                catch (Exception)
+                else
                 {
-                    throw;
+                    MessageBox.Show("Error");
                 }
+
             }
-            else
+            catch (Exception ex) when (ex is HttpRequestException || ex is SocketException || ex is WebException)
             {
-                MessageBox.Show("Error");
+                MessageBox.Show("Application gonna restart");
+                Process.Start(Assembly.GetExecutingAssembly().Location);
+                Environment.Exit(0);
+                throw;
+            }
+            catch (Exception)
+            {
+                throw;
             }
         }
 
         public async Task<bool> DeleteTask(Models.Task taskToDelete)
         {
-            //var t = GetAllTasks();
-            //var tasks = await GetAllTasks().Where(x => x.TaskName != taskToDelete.TaskName && x.TaskDesc != taskToDelete.TaskDesc && x.HourTask != taskToDelete.HourTask);           
-            //SaveAll(new ObservableCollection<Models.Task>(tasks));
-            //return GetAllTasks();
-
-
-            using (HttpClient client = new HttpClient())
+            try
             {
-                try
+                var content = new StringContent(JsonConvert.SerializeObject(taskToDelete), Encoding.UTF8, "application/json");
+                var response = await client.DeleteAsync($"{client.BaseAddress}?taskId={taskToDelete.TaskId}").ConfigureAwait(false);
+                if (response.EnsureSuccessStatusCode().StatusCode == System.Net.HttpStatusCode.OK)
                 {
-                    string url = $"http://localhost:3000/tasks?taskId={taskToDelete.TaskId}";
-                    var content = new StringContent(JsonConvert.SerializeObject(taskToDelete), Encoding.UTF8, "application/json");
-                    var response = await client.DeleteAsync(url).ConfigureAwait(false);
-                    response.EnsureSuccessStatusCode();
-                    if (response.IsSuccessStatusCode)
-                    {
-                        client.Dispose();
-                        return true;
-                    }
-
-                    return false;
-
+                    //client.Dispose();
+                    return true;
                 }
-                catch (Exception)
-                {
-
-                    throw;
-                }
+                return false;
             }
-            throw new Exception();
+            catch (Exception ex) when (ex is HttpRequestException || ex is SocketException || ex is WebException)
+            {
+                MessageBox.Show("Application gonna restart");
+                Process.Start(Assembly.GetExecutingAssembly().Location);
+                Environment.Exit(0);
+                throw;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
